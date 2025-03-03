4ec31992c8e8a44ba0fc7be670d8794d42f7b751
Scott McCrindle <iainmccrindle@gmail.com>
2025-03-02 23:10:18 -0500

    Completed all functionality including adding anchor to box on click

--- lesson-7/js/ar-example.js
+++ lesson-7/js/ar-example.js
@@ -85,6 +85,8 @@ const createScene = async function() {
 
     // STEP 3: Commit your code and push it to a server, then try it out with a headset - notice how the orange box is right at your feet - 0, 0, 0 is located on the floor at your feet
 
+    // STEP 9: Change the reference space type (in the optionalFeatures array above) to "local-floor", so the box is placed on the floor, not at your feet, or to "local", so the box is placed in front of you, or "unbounded", so the box is placed anywhere in the world.
+
 
     /* HIT-TEST
     ---------------------------------------------------------------------------------------------------- */
