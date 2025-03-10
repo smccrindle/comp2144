658596056e4d7d5e7db5b6666c569e47ecf17a49
Scott McCrindle <iainmccrindle@gmail.com>
2025-03-09 21:56:22 -0400

    Changing to local-floor

--- lesson-8/js/ar-example.js
+++ lesson-8/js/ar-example.js
@@ -58,7 +58,7 @@ const createScene = async function() {
     const xr = await scene.createDefaultXRExperienceAsync({
         uiOptions: {
             sessionMode: "immersive-ar",
-            referenceSpaceType: "unbounded", // viewer, local, local-floor, bounded-floor, or unbounded (https://developer.mozilla.org/en-US/docs/Web/API/XRReferenceSpace and https://gist.github.com/lempa/64b3a89a19cbec980ade709be35d7cbc#file-webxr-reference-space-types-csv)
+            referenceSpaceType: "local-floor", // viewer, local, local-floor, bounded-floor, or unbounded (https://developer.mozilla.org/en-US/docs/Web/API/XRReferenceSpace and https://gist.github.com/lempa/64b3a89a19cbec980ade709be35d7cbc#file-webxr-reference-space-types-csv)
             overlay: {
                 element: document.getElementById("rangeOverlay")
             }
