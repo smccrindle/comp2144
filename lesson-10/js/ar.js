979674a5ee507290fc0811aa62ac87314ea80ec0
Scott McCrindle <iainmccrindle@gmail.com>
2025-03-15 17:52:03 -0400

    Oops - scaling, not scale as a property of a mesh

--- lesson-10/js/ar.js
+++ lesson-10/js/ar.js
@@ -75,10 +75,10 @@ const createScene = async function() {
     // box.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, box, "visibility", 0.2, 1000)).then(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, box, "visibility", 1.0, 1000));
 
     // STEP XX: Scale the mesh slightly on hover (BABYLON.ActionManager.OnPointerOverTrigger)
-    box.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPointerOverTrigger, box, "scale", 1.2, 1000)).then(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPointerOutTrigger, box, "scale", 1.0, 1000));
+    box.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPointerOverTrigger, box, "scaling", 1.2, 1000)).then(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPointerOutTrigger, box, "scaling", 1.0, 1000));
 
     // STEP XX: Change the color of the mesh on click (BABYLON.ActionManager.OnPickTrigger)
-    // box.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, box, "material.diffuseColor", BABYLON.Color3.Random(), 1000));
+    box.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, box, "material.diffuseColor", BABYLON.Color3.Random(), 1000));
 
     // box.material.diffuseColor = BABYLON.Color3.Random();
 
