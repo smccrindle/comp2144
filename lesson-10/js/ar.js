abced8969206973e538e7f44afef194af7d87b44
Scott McCrindle <iainmccrindle@gmail.com>
2025-03-16 20:56:58 -0400

    Removed code from lesson, leaving only relevant numbered steps

--- lesson-10/js/ar.js
+++ lesson-10/js/ar.js
@@ -1,11 +1,3 @@
-/* LESSON TODO
- * 1. Hover action DONE
- * 2. Click action DONE
- * 3. Grab and drag behaviour DONE
- * 4. Native GUI controls
- * 5. Plane detection (maybe leave this in Lesson 9?)
-
-*/
 // Get the canvas element as a const
 const canvas = document.getElementById("renderCanvas");
 // Create the BABYON 3D engine, and attach it to the canvas
@@ -48,14 +40,10 @@ const createScene = async function() {
     box.material = boxMat;
     // The initial position of the box is 0, 0, 0 so with the referenceSpaceType: "unbounded" it will be located on the viewer's head, which is the origin point of the scene - reposition the box as you'd like
     // box.position.y = 0.5;
-    box.position.z = 0.5;
+    // box.position.z = 0.5;
 
     // STEP 7: Let's create another native mesh object for interactive purposes
-    const can = BABYLON.MeshBuilder.CreateCylinder("can", {diameter: 0.1, height: 0.3, tessellation: 10});
-    const canMat = new BABYLON.StandardMaterial("canMat");
-    canMat.diffuseColor = new BABYLON.Color3(1, 0, 0.6);
-    can.material = canMat;
-    can.position.x = 0.5;
+    
 
     /* SOUNDS
     ---------------------------------------------------------------------------------------------------- */
@@ -72,7 +60,7 @@ const createScene = async function() {
         uiOptions: {
             sessionMode: "immersive-ar",
             // STEP 1: Set the referenceSpaceType to "unbounded" - since the headset is in passthrough mode with AR, let the vistor go anywhere they like within their physical space
-            referenceSpaceType: "unbounded" // viewer, local, local-floor, bounded-floor, or unbounded (https://developer.mozilla.org/en-US/docs/Web/API/XRReferenceSpace and https://gist.github.com/lempa/64b3a89a19cbec980ade709be35d7cbc#file-webxr-reference-space-types-csv)
+            referenceSpaceType: "local" // viewer, local, local-floor, bounded-floor, or unbounded (https://developer.mozilla.org/en-US/docs/Web/API/XRReferenceSpace and https://gist.github.com/lempa/64b3a89a19cbec980ade709be35d7cbc#file-webxr-reference-space-types-csv)
 
         },
         // Enable optional features - either all of them with true (boolean), or as an array
@@ -83,62 +71,43 @@ const createScene = async function() {
     /* INTERACTION
     ---------------------------------------------------------------------------------------------------- */
     // STEP 2: Add an action manager to the box mesh
-    box.actionManager = new BABYLON.ActionManager(scene);
-
+    
 
     // STEP 3a: Set up a "mouseover" effect - register a new action with the registerAction() method
-    box.actionManager.registerAction(
+    
         // STEP 3b: Set up the action to animate the effect with InterpolateValueAction
-        new BABYLON.InterpolateValueAction(
+        
             // STEP 3c: Add a hover action with OnPointerOverTrigger, to scale the box 1.2 times its size over a quarter of a second
-            BABYLON.ActionManager.OnPointerOverTrigger,
-            box,
-            "scaling",
-            new BABYLON.Vector3(1.2, 1.2, 1.2),
-            250
-        )
-    );
+
     
     // STEP 4a: Set up a "mouseout" effect - register another action with the registerAction() method
-    box.actionManager.registerAction(
+    
         // STEP 4b: Set up the action to animate the effect once again with InterpolateValueAction
-        new BABYLON.InterpolateValueAction(
+        
             // STEP 4c: Add a hover-out action with OnPointerOutTrigger, to scale the box back to its original size over a quarter of a second
-            BABYLON.ActionManager.OnPointerOutTrigger,
-            box,
-            "scaling",
-            new BABYLON.Vector3(1, 1, 1),
-            250
-        )
-    );
+            
 
     // STEP 5a: Set up a "click" effect - register a third action
-    // box.actionManager.registerAction(
-    //     //STEP 5b: Set up the action to change the color value
-    //     new BABYLON.SetValueAction(
-    //         // STEP 5c: Add a click action, and use a random color
-    //         BABYLON.ActionManager.OnPickTrigger, box, "material.diffuseColor", BABYLON.Color3.Random()
-    //     )
-    // );
+    
+        //STEP 5b: Set up the action to change the color value
+
+            // STEP 5c: Add a click action, and use a random color
+            
     // STEP 5d: Notice how you can only change the color of the box once - if we'd like to do it every time we click on the box, we'd have to re-register the action again and again - comment out the above STEP 5 code
 
+
     // STEP 6a: Instead, let's register one action to run some code on each click - this will side-step the issue
-    box.actionManager.registerAction(
+    
         // STEP 6b: Add a new BABYLON.ExecuteCodeAction
-        new BABYLON.ExecuteCodeAction(
+        
             // STEP 6c: Add a OnPickTrigger that references a function called changeBoxColor
-            BABYLON.ActionManager.OnPickTrigger,
-            changeBoxColor
-        )
-    );
+            
 
     // STEP 6d: Build a simple function to change the material.diffuseColor of the box to a random color
-    function changeBoxColor() {
-        box.material.diffuseColor = BABYLON.Color3.Random();
-    };
+    
 
     // STEP 8: Make the can grabbable and moveable (awesome)! 
-    can.bakeCurrentTransformIntoVertices().addBehavior(new BABYLON.SixDofDragBehavior());
+    
 
 
     // Return the scene
