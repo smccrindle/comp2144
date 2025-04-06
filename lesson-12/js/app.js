c80f06345b5916f411bf64b392d9a9a8a6b6738b
Scott McCrindle <iainmccrindle@gmail.com>
2025-04-06 09:58:00 -0400

    Trying to access manufacturer from BT device

--- lesson-12/js/app.js
+++ lesson-12/js/app.js
@@ -4,7 +4,7 @@ document.getElementById('connectButton').addEventListener('click', () => {
     // Request a Bluetooth device
     navigator.bluetooth.requestDevice({
         acceptAllDevices: true,
-        optionalServices: ['battery_service'] // array of services
+        optionalServices: ['battery_service', 'device_information'] // array of services
     })
     .then(device => {
         statusElement.textContent = 'Connecting to ' + device.name + '...';
@@ -12,18 +12,22 @@ document.getElementById('connectButton').addEventListener('click', () => {
     })
     .then(server => {
         statusElement.textContent = 'Connected. Getting Battery Service...';
-        return server.getPrimaryService('battery_service');
+        // return server.getPrimaryService('battery_service');
+        return server.getPrimaryService('device_information');
     })
     .then(service => {
-        statusElement.textContent = 'Getting Battery Level Characteristic...';
-        return service.getCharacteristic('battery_level');
+        // statusElement.textContent = 'Getting Battery Level Characteristic...';
+        // return service.getCharacteristic('battery_level');
+        statusElement.textContent = 'Getting Manufacturer Characteristic...';
+        return service.getCharacteristic('manufacturer_name_string');
     })
     .then(characteristic => {
         return characteristic.readValue();
     })
     .then(value => {
-        const batteryLevel = value.getUint8(0);
-        statusElement.textContent = 'Battery Level: ' + batteryLevel + '%';
+        // const batteryLevel = value.getUint8(0);
+        // statusElement.textContent = 'Battery Level: ' + batteryLevel + '%';
+        statusElement.textContent = `Manufacturer: ${value}`;
     })
     .catch(error => {
         statusElement.textContent = 'Error: ' + error;
